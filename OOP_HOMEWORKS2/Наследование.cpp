82db78061813989a6a6407b4e9632498b64525df
Mira Madani <miramadani2701@gmail.com>
2023-10-05 12:57:25 +0300

    done homework

--- OOP_HOMEWORKS2/Наследование.cpp
+++ OOP_HOMEWORKS2/Наследование.cpp
@@ -1,5 +1,4 @@
-//1) Создайте класс Transport(транспортное средство) и его дочерние классы Автомобиль, Повозка, Велосипед.
-//Создайте методы для расчета времени и стоимости поездки.
+
 //2) Создайте класс Passport(паспорт), который будет содержать паспортную информацию о гражданине Украины.
 //С помощью механизма наследования реализуйте класс ForeignPassport(загранпаспорт), производный от Passport.
 //Напомним, что загранпаспорт содержит помимо паспортных данных также данные о визах, номер загранпаспорта.
@@ -7,83 +6,6 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-class Transport {
-protected:
-	int speed;
-	double distance;
-	int cost_per_mile;
-	 
-public:
-	Transport() {
-		speed = 0;
-		distance = 0;
-		cost_per_mile = 0;
-	}
-	Transport(int a,int b,int c) {
-		speed = a;
-		distance = b;
-		cost_per_mile = c;
-	}
-	void Time() {
-		cout <<"Time in secondds to pass "<<distance<<" miles: " << distance / speed*1000;
-		cout << endl;
-	}
-	void Cost() {
-		cout <<"Money to spent: " << cost_per_mile * distance;
-		cout << endl;
-	}
-};
-
-class Car : public Transport
-{
-	string firma;
-	string color;
-public:
-	Car() {
-		firma = "BMW";
-		color = "red";
-	}
-	Car(int a ,int b,int c,string a1, string b1): Transport(a,b,c)
-	{
-		firma = a1;
-		color=b1;
-	}
-};
-
-class Cart : public Transport
-{
-	string color;
-public:
-	Cart() {
-		color = "red";
-	}
-	Cart(int a, int b, int c,  string b1) : Transport(a, b, c)
-	{
-		color = b1;
-	}
-};
-
-class Bike : public Transport
-{
-	string firma;
-	string color;
-	int wheel_count;
-public:
-	Bike() {
-		firma = "BMW";
-		color = "red";
-		wheel_count = 0;
-	}
-	Bike(int a, int b, int c,int d, string a1, string b1) : Transport(a, b, c)
-	{
-		firma = a1;
-		color = b1;
-		wheel_count = d;
-	}
-};
-
-
 
 
 class Passport {
@@ -128,19 +50,7 @@ public:
 	}
 };
 int main() {
-	//1
-	/*Car car1(60,7,60,"Audi","blue");
-	Cart cart1(3, 7, 5, "brown");
-	Bike bike1(18, 7, 0 ,2, "CITYBIKE", "yellow");
-
-	car1.Cost();
-	cart1.Cost();
-	bike1.Cost();
-
-	car1.Time();
-	cart1.Time();
-	bike1.Time();*/
-
+	
 	//2
 
 	ForeignPassport passport1("Mira", "Madani", "Abd", 1, 2, 1224944322);
